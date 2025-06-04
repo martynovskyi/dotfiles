90bc08cb1f688d389231d7e9085077b674c77e98
Vitalii Martynovskyi <vitalii@motokyi.com>
2025-06-04 13:24:34 +0300

    config(neovim): line numbers & show hidden files

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,4 +1,5 @@
 vim.cmd("set expandtab")
+vim.cmd("set number")
 vim.cmd("set tabstop=4")
 vim.cmd("set softtabstop=4")
 vim.cmd("set shiftwidth=4")
--- .config/nvim/lua/plugins/neo-tree.lua
+++ .config/nvim/lua/plugins/neo-tree.lua
@@ -1,15 +1,20 @@
 return {
-  "nvim-neo-tree/neo-tree.nvim",
-  branch = "v3.x",
-  dependencies = {
-    "nvim-lua/plenary.nvim",
-    "nvim-tree/nvim-web-devicons",
-    "MunifTanjim/nui.nvim",
-  },
-  opts = {
-    -- fill any relevant options here
-  },
-  config = function()
-    vim.keymap.set('n', '<C-n>', ':Neotree filesystem reveal left<CR>', {})
-  end,
+    "nvim-neo-tree/neo-tree.nvim",
+    branch = "v3.x",
+    dependencies = {
+        "nvim-lua/plenary.nvim",
+        "nvim-tree/nvim-web-devicons",
+        "MunifTanjim/nui.nvim",
+    },
+    opts = {
+        filesystem = {
+            filtered_items = {
+                visible = true,
+            },
+        },
+    },
+    config = function(_, opts)
+        require("neo-tree").setup(opts)
+        vim.keymap.set('n', '<C-n>', ':Neotree filesystem reveal left<CR>', {})
+    end,
 }
