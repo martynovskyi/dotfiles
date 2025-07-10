383786240f3481e4f9c93423ef44af8a0255a094
Vitalii Martynovskyi <vitalii@motokyi.com>
2025-07-10 21:20:40 +0300

    feat: add diagnostic virtual text

--- .config/nvim/lua/config/lsp.lua
+++ .config/nvim/lua/config/lsp.lua
@@ -2,3 +2,10 @@ vim.lsp.enable({
     "lua-ls",
     "rust-analyzer",
 })
+
+vim.diagnostic.config({
+  virtual_text = true,
+  virtual_lines = { current_line = true},
+  underline = true,
+  update_in_insert = false
+})
